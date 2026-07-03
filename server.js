6f1900bdad820b5a15c73b85f27c12af7b8bbf63
huynhtuongngan123-web <huynhtuongngan123@gmail.com>
2026-07-03 15:56:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ import { toFile } from "openai/uploads";
 dotenv.config();
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 const upload = multer({
   storage: multer.memoryStorage(),
